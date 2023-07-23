d99af1dba77491294b4f0ebe8d09e76fe716e24e
phattarapon <phattarapon.cpe61346@gmail.com>
2023-07-24 00:06:35 +0700

    Initial Commit

---
+++ logic_test_scg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  logic_test_scg
+//
+//  Created by Phattarapon Jungtakarn on 24/7/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
